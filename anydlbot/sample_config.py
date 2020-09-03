762c266879a6368d9bc84df4b221d9fb11fd339b
mojitaktir <mojitaktir@gmail.com>
2020-09-03 11:31:19 +0430

    Update sample_config.py

--- anydlbot/sample_config.py
+++ anydlbot/sample_config.py
@@ -5,11 +5,11 @@ class Config(object):
     # get a token from @BotFather
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1217999322:AAH-C6S9VeoJmUe5aO3HhLIfHUYTCr-aHqU")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = int(os.environ.get("APP_ID", 6))
+    API_HASH = os.environ.get("eb06d4abfb49dc3eeb1aeb98ae0f581e")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "7351948").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
